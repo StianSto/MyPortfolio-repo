50faab3b22897c229b5955f690ff66227a89dff8
StianSto <95475303+StianSto@users.noreply.github.com>
2022-08-10 14:45:16 +0200

    added custom property and js function to help with a bug ocurring when rotating en element on hero

--- js/index.js
+++ js/index.js
@@ -29,3 +29,24 @@ window.onbeforeunload = function () {
     window.scrollTo(0, 0);
   }
 
+
+
+
+function heroBottomRotateFix () {
+
+    const heroBottom = document.querySelector(".hero--bottom");
+    var currentViewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    
+    const heroBottomStyles = window.getComputedStyle(heroBottom)
+    var heroRotateDeg = heroBottomStyles.getPropertyValue("--heroRotate");
+
+    
+    const calculateTan = Math.tan(parseFloat(heroRotateDeg) * Math.PI / 180) * currentViewportWidth;
+    const lengthTan = Math.abs(calculateTan);
+    heroBottom.style.setProperty("--hero-margin", lengthTan + "px")
+
+    console.log(heroRotateDeg)
+
+}
+heroBottomRotateFix();
+window.onresize = heroBottomRotateFix;

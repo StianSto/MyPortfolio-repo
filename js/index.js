b69eae7a7952f52d7be277a197b7c7d93b719dde
StianSto <95475303+StianSto@users.noreply.github.com>
2022-08-17 15:53:40 +0200

    merge recent edits on macbook.

--- js/index.js
+++ js/index.js
@@ -7,20 +7,14 @@ console.log(window.innerHeight);
 
 window.addEventListener('scroll', function parallax() {
 
-    console.log("scroll:" + window.scrollY);
-    console.log("phone is:" + phoneVector[0].offsetTop);
-
     var value = window.scrollY; 
     var parallaxPhone = (value / 76.8) * -4;
     var parallaxPC = value * -0.05;
-
 
     phoneVector[0].style.top = 90 + parallaxPhone + "%";
     phoneVector[1].style.top = 110 + parallaxPhone + "%";
     macVector[0].style.top = 80 + parallaxPC + "px";
     macVector[1].style.top = 100 + parallaxPC + "px";
-
-    // macVector.style.top = parallaxPhone * 2;
     }
 );
 
@@ -38,14 +32,11 @@ function heroBottomRotateFix () {
     var currentViewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
     
     const heroBottomStyles = window.getComputedStyle(heroBottom)
-    var heroRotateDeg = heroBottomStyles.getPropertyValue("--heroRotate");
-
+    var heroRotateDeg = heroBottomStyles.getPropertyValue("--hero-rotate");
     
     const calculateTan = Math.tan(parseFloat(heroRotateDeg) * Math.PI / 180) * currentViewportWidth;
     const lengthTan = Math.abs(calculateTan);
     heroBottom.style.setProperty("--hero-margin", lengthTan + "px")
-
-    console.log(heroRotateDeg)
 
 }
 heroBottomRotateFix();
